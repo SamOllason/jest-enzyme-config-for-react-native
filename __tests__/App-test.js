620c3a6f82a68fbc02ba3b9afaf847a943abc6f9
Sam Ollason <Sam_ollason@hotmail.co.uk>
2019-05-14 09:27:07 +0100

    Added jest-enzyme and tests

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -4,11 +4,23 @@
 
 import 'react-native';
 import React from 'react';
+import { shallow } from 'enzyme';
 import App from '../App';
 
-// Note: test renderer must be required after react-native.
+// Note: this is just for use with Jest snapshot testing
+// and comes packaged with react-native init project.
+// You do not need this if using Enzyme 'toMatchSnapshot' etc.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
+// This test just uses Jest snapshot testing
+it('renders correctly, test using Jest', () => {
   renderer.create(<App />);
+});
+
+
+// Using Jest + Enzyme
+describe('<App />', () => {
+  it('renders correctly, test using Jest + Enzyme', () => {
+    expect(shallow(<App/>)).toMatchSnapshot();
+  });
 });
